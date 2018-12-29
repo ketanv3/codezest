90f3e51fed884c3749cafae263a97787231ae378
Ketan Verma <ketan9495@gmail.com>
2018-12-29 21:55:29 +0530

    Do not ignore dist folder

---
+++ dist/precache-manifest.eaf319f3a17eaacfeac4a23f11154132.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = [
+  {
+    "revision": "89b5dbc836aeee3e58f68a433fd14d2e",
+    "url": "/fonts/museosansrounded-100-webfont.eot"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "42f36c7f8641a98f46124352f946fc57",
+    "url": "/fonts/museosansrounded-100-webfont.woff"
+  },
+  {
+    "revision": "23b27199d95bb408c575",
+    "url": "/js/chunk-vendors.f7a64673.js"
+  },
+  {
+    "revision": "543d1f9279e03157196e",
+    "url": "/js/details.e5638b98.js"
+  },
+  {
+    "revision": "b99e46a86d77d6cf1b1fb38a46065340",
+    "url": "/fonts/museosansrounded-100-webfont.svg"
+  },
+  {
+    "revision": "ce17d40097e16688c12fe1a00cfdded1",
+    "url": "/img/skyline.ce17d400.svg"
+  },
+  {
+    "revision": "a50556ec5d4cf6501ee42804fc31656e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2446a4e2ae583b54dd95f4d0d90f5c8",
+    "url": "/img/spinner.c2446a4e.svg"
+  },
+  {
+    "revision": "4ea383e5f774764602ff",
+    "url": "/js/app.aa08cba6.js"
+  },
+  {
+    "revision": "13741a53ae1a9dde5620eb804d4f21b7",
+    "url": "/fonts/museosansrounded-1000-webfont.svg"
+  },
+  {
+    "revision": "4ea383e5f774764602ff",
+    "url": "/css/app.86d7659f.css"
+  },
+  {
+    "revision": "47f2122376c169619122bfa7c4d94f56",
+    "url": "/fonts/museosansrounded-900-webfont.ttf"
+  },
+  {
+    "revision": "2f54b648150616ce7016ac232f3c3de4",
+    "url": "/fonts/museosansrounded-900-webfont.svg"
+  },
+  {
+    "revision": "83974329b437b700d2f74f01f1e6e3b4",
+    "url": "/fonts/museosansrounded-900-webfont.woff"
+  },
+  {
+    "revision": "1a536994dd372da7280cce25122442b3",
+    "url": "/fonts/museosansrounded-700-webfont.woff"
+  },
+  {
+    "revision": "fad115eeac67321596fc4d028b0b5385",
+    "url": "/fonts/museosansrounded-700-webfont.svg"
+  },
+  {
+    "revision": "322a00046ac49170505e2fc09db1ba6c",
+    "url": "/fonts/museosansrounded-900-webfont.eot"
+  },
+  {
+    "revision": "63a7137485d64930fbe95c2aa3bb7dd1",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "57451ac30d6108c10fc133ea14f44d17",
+    "url": "/fonts/museosansrounded-700-webfont.ttf"
+  },
+  {
+    "revision": "135d1a023c0020b68aa61df4f418d4c2",
+    "url": "/favicon-16x16.png"
+  },
+  {
+    "revision": "15033b6695eb5629e694d5ed76a88b18",
+    "url": "/fonts/font.css"
+  },
+  {
+    "revision": "99d9df7879055b6c8f4ad618944811e3",
+    "url": "/fonts/museosansrounded-100-webfont.ttf"
+  },
+  {
+    "revision": "d31d1fba074ede1269b8e10be338184b",
+    "url": "/fonts/museosansrounded-500-webfont.woff"
+  },
+  {
+    "revision": "543d1f9279e03157196e",
+    "url": "/css/details.e864030c.css"
+  },
+  {
+    "revision": "20cbce6c1820b04beaa76e08434b2a6b",
+    "url": "/fonts/museosansrounded-1000-webfont.eot"
+  },
+  {
+    "revision": "33ee0df410b955564a25bf097910946c",
+    "url": "/fonts/museosansrounded-500-webfont.svg"
+  },
+  {
+    "revision": "98e33b9976da7d91eacb104c783a59bc",
+    "url": "/fonts/museosansrounded-1000-webfont.woff"
+  },
+  {
+    "revision": "85f44f6aa8a78eace06fff2d9b9f2007",
+    "url": "/fonts/museosansrounded-500-webfont.ttf"
+  },
+  {
+    "revision": "86348d76f2d30588ddd94c27aa6f0e69",
+    "url": "/fonts/museosansrounded-1000-webfont.ttf"
+  },
+  {
+    "revision": "e6e584bb3b4eb1a4315c4bfa4e071a6f",
+    "url": "/fonts/museosansrounded-300-webfont.eot"
+  },
+  {
+    "revision": "0f2cb4989a4e31bdf6268b9a398bc752",
+    "url": "/fonts/museosansrounded-300-webfont.svg"
+  },
+  {
+    "revision": "0c164c88fe794d8e4699f72bca3f60f9",
+    "url": "/fonts/museosansrounded-300-webfont.ttf"
+  },
+  {
+    "revision": "273752ecca910d63791c1168185409ef",
+    "url": "/fonts/museosansrounded-300-webfont.woff"
+  },
+  {
+    "revision": "e4c2534fb0fec6cb738970d814f41ffd",
+    "url": "/fonts/museosansrounded-700-webfont.eot"
+  },
+  {
+    "revision": "333d3129653acf0f72e3a4e16bf6efea",
+    "url": "/fonts/museosansrounded-500-webfont.eot"
+  },
+  {
+    "revision": "1c1a2deb041b16d210496a88353bd112",
+    "url": "/api/touchlock.json"
+  },
+  {
+    "revision": "df5f2450f51264636eebb075607770a2",
+    "url": "/api/prope.json"
+  },
+  {
+    "revision": "6f39b0a5efa9e79465dff54f1787ce9e",
+    "url": "/api/sightcontrol.json"
+  },
+  {
+    "revision": "7f4c7484002361af28317e2efac4e7c8",
+    "url": "/api/zipzapdeals.json"
+  },
+  {
+    "revision": "8a880bd672e8ef4bac1398d4e062c9c1",
+    "url": "/api/template.json"
+  },
+  {
+    "revision": "44b3e7095615efa216631b741872d577",
+    "url": "/api/rootchecker.json"
+  },
+  {
+    "revision": "29993a1ea14fb95039b3b8fe53cdb8c7",
+    "url": "/api/scribble.json"
+  },
+  {
+    "revision": "e9d4738cdee0e283f0c9d64d22e384db",
+    "url": "/api/tap5ive.json"
+  },
+  {
+    "revision": "cf4b698d5bec52f4c8d233996a7fa30b",
+    "url": "/api/items.json"
+  },
+  {
+    "revision": "eb7c7abcc008f365a81434dfaf0afe5e",
+    "url": "/api/beatbook.json"
+  },
+  {
+    "revision": "804fc3056d0ff47ccd4fffff096d6560",
+    "url": "/api/amizowned.json"
+  }
+];
